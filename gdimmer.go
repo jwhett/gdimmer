c373b310a3b6e81058a710bae0fc77fa9932a855
Josh Whetton <whetton.josh@gmail.com>
2018-08-22 14:12:35 -0400

    Handling everything as ints again..

--- gdimmer.go
+++ gdimmer.go
@@ -7,8 +7,9 @@ import (
 )
 
 type Dimmer struct {
-    max     string
-    current string
+    // Struct to represent the state of a screen dimmer.
+    max     int
+    current int
 }
 
 func check(e error) {
@@ -18,35 +19,32 @@ func check(e error) {
 }
 
 func New() (*Dimmer) {
+    // Initialize a new dimmer with the system's current settings.
+
+    // Take the []bytes from the "current brightness" file
+    // and turn it into an integer.
     c, err := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/brightness")
     check(err)
+    cur := strings.TrimSpace(string(c))
+    current, _ := strconv.Atoi(cur)
 
+    // Take the []bytes from the "max brightness" file
+    // and turn it into an integer.
     m, err := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/max_brightness")
     check(err)
-
-    cur := strings.TrimSpace(string(c))
-    check(err)
-
     mx := strings.TrimSpace(string(m))
-    check(err)
+    max, _ := strconv.Atoi(mx)
 
-    return &Dimmer{max: mx, current: cur}
+    return &Dimmer{max: max, current: current}
 }
 
-func (d *Dimmer) Max() string {
+func (d *Dimmer) Max() int {
+    // Return the maximum brightness.
     return d.max
 }
 
-func (d *Dimmer) Current() string {
+func (d *Dimmer) Current() int {
+    // Return the current brightness.
     return d.current
 }
 
-func (d *Dimmer) Maxasi() int {
-    imax, _ := strconv.Atoi(d.max)
-    return imax
-}
-
-func (d *Dimmer) Currentasi() int {
-    icur, _ := strconv.Atoi(d.current)
-    return icur
-}
--- gdimmer_test.go
+++ gdimmer_test.go
@@ -4,19 +4,26 @@ import (
     "github.com/jwhett/gdimmer"
     "io/ioutil"
     "strings"
+    "strconv"
     "testing"
 )
 
 func TestInit(t *testing.T) {
     d := gdimmer.New()
-    m, _ := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/max_brightness")
-    c, _ := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/brightness")
 
-    if d.Max() != strings.TrimSpace(string(m)) {
+    m, _ := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/max_brightness")
+    mx := strings.TrimSpace(string(m))
+    max, _ := strconv.Atoi(mx)
+
+    c, _ := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/brightness")
+    cur := strings.TrimSpace(string(c))
+    current, _ := strconv.Atoi(cur)
+
+    if d.Max() != max {
         t.Error("Max not set properly...")
     }
 
-    if d.Current() != strings.TrimSpace(string(c)) {
+    if d.Current() != current {
         t.Error("Current not set properly...")
     }
 }
