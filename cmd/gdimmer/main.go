cf332813c0ef7cc39f2c24983081360a2b4eea0f
Josh Whetton <whetton.josh@gmail.com>
2018-08-23 11:21:09 -0400

    Changed how we're setting command line flags.
    Also adding the ability to explicitly set the brightness to a value.

--- cmd/gdimmer/main.go
+++ cmd/gdimmer/main.go
@@ -6,20 +6,31 @@ import (
 )
 
 func main() {
-	up   := flag.Bool("up", false, "Turn up the brightness.")
-	down := flag.Bool("down", false, "Turn down the brightness.")
-	max  := flag.Bool("max", false, "Maximum brightness.")
+
+    var (
+        up, down, max, force bool
+        set int
+    )
+
+	flag.BoolVar(&up, "up", false, "Turn up the brightness.")
+	flag.BoolVar(&down, "down", false, "Turn down the brightness.")
+	flag.BoolVar(&max, "max", false, "Maximum brightness.")
+	flag.BoolVar(&force, "f", false, "Force; to be used with 'set'.")
+	flag.IntVar(&set, "set", 1, "Explicitly set brightness to VALUE.")
 	flag.Parse()
 
     d := gd.New("gmux_backlight")
 
-	if *up {
+	if up {
     	d.StepUp()
 	}
-	if *down {
+	if down {
     	d.StepDown()
 	}
-	if *max {
+	if max {
     	d.SetBrightness(d.GetMax())
+	}
+	if force {
+    	d.SetBrightness(set)
 	}
 }
