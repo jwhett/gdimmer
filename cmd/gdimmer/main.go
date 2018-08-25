5c92b5b35e73747225ef9d99603d55449ad529ba
Josh Whetton <whetton.josh@gmail.com>
2018-08-25 13:47:20 -0400

    Provider can be set as environment variable.
    BLPROVIDER environment variable will be checked to alleviate
    setting the provider with the flag.

--- cmd/gdimmer/main.go
+++ cmd/gdimmer/main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-    "flag"
-    gd "github.com/jwhett/gdimmer"
+	"flag"
+	gd "github.com/jwhett/gdimmer"
+	"os"
 )
 
 func main() {
 
-    var (
-        up, down, max, force bool
-        provider string
-        set int
-    )
+	var (
+		up, down, max, force bool
+		provider             string
+		set                  int
+	)
+
+	if len(os.Getenv("BLPROVIDER")) > 0 {
+		provider = os.Getenv("BLPROVIDER")
+	} else {
+		flag.StringVar(&provider, "provider", "gmux_backlight", "Chose backlight provider. Defaults to GMUX (Macintosh). Find your provider in /sys/class/backlight/.")
+		flag.StringVar(&provider, "p", "gmux_backlight", "Chose backlight provider. Defaults to GMUX (Macintosh). Find your provider in /sys/class/backlight/.")
+	}
 
 	flag.BoolVar(&up, "up", false, "Turn up the brightness.")
 	flag.BoolVar(&up, "u", false, "Turn up the brightness.")
@@ -19,26 +27,24 @@ func main() {
 	flag.BoolVar(&down, "d", false, "Turn down the brightness.")
 	flag.BoolVar(&max, "max", false, "Maximum brightness.")
 	flag.BoolVar(&max, "m", false, "Maximum brightness.")
-	flag.StringVar(&provider, "provider", "gmux_backlight", "Chose backlight provider. Defaults to GMUX (Macintosh). Find your provider in /sys/class/backlight/.")
-	flag.StringVar(&provider, "p", "gmux_backlight", "Chose backlight provider. Defaults to GMUX (Macintosh). Find your provider in /sys/class/backlight/.")
 	flag.BoolVar(&force, "force", false, "Force; to be used with 'set'.")
 	flag.BoolVar(&force, "f", false, "Force; to be used with 'set'.")
 	flag.IntVar(&set, "set", 1, "Explicitly set brightness to VALUE.")
 	flag.IntVar(&set, "s", 1, "Explicitly set brightness to VALUE.")
 	flag.Parse()
 
-    d := gd.New(provider)
+	d := gd.New(provider)
 
 	if up {
-    	d.StepUp()
+		d.StepUp()
 	}
 	if down {
-    	d.StepDown()
+		d.StepDown()
 	}
 	if max {
-    	d.SetBrightness(d.GetMax())
+		d.SetBrightness(d.GetMax())
 	}
 	if force {
-    	d.SetBrightness(set)
+		d.SetBrightness(set)
 	}
 }
