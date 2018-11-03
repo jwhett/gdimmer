1b3179e13f1580da37a4010b24cbd84c3dca6568
Josh Whetton <whetton.josh@gmail.com>
2018-11-03 13:46:01 -0400

    Added error handling for missing backlight files in testing.

--- gdimmer_test.go
+++ gdimmer_test.go
@@ -1,29 +1,35 @@
 package gdimmer_test
 
 import (
-    "github.com/jwhett/gdimmer"
-    "io/ioutil"
-    "strings"
-    "strconv"
-    "testing"
+	"github.com/jwhett/gdimmer"
+	"io/ioutil"
+	"strconv"
+	"strings"
+	"testing"
 )
 
 func TestInit(t *testing.T) {
-    d := gdimmer.New("gmux_backlight")
+	d := gdimmer.New("gmux_backlight")
 
-    m, _ := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/max_brightness")
-    mx := strings.TrimSpace(string(m))
-    max, _ := strconv.Atoi(mx)
+	m, err := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/max_brightness")
+	if err != nil {
+		t.Skip("Unable to read brightness files.")
+	}
+	mx := strings.TrimSpace(string(m))
+	max, _ := strconv.Atoi(mx)
 
-    c, _ := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/brightness")
-    cur := strings.TrimSpace(string(c))
-    current, _ := strconv.Atoi(cur)
+	c, err := ioutil.ReadFile("/sys/class/backlight/gmux_backlight/brightness")
+	if err != nil {
+		t.Skip("Unable to read brightness files.")
+	}
+	cur := strings.TrimSpace(string(c))
+	current, _ := strconv.Atoi(cur)
 
-    if d.GetMax() != max {
-        t.Error("Max not set properly...")
-    }
+	if d.GetMax() != max {
+		t.Error("Max not set properly...")
+	}
 
-    if d.GetCurrent() != current {
-        t.Error("Current not set properly...")
-    }
+	if d.GetCurrent() != current {
+		t.Error("Current not set properly...")
+	}
 }
